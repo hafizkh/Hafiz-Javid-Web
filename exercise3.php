a2e6f35667aa0f4db6f66089de8aa53d540748e9
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-02-02 19:45:09 +0200

    Solved few questions in the assignment

---
+++ exercise3.php
@@ -0,0 +1,70 @@
+<?php
+
+echo "---------Assignment 3------------ <br>";
+
+echo "<br>";
+
+echo "---------Hafiz Javid-------------";
+echo "<br>";
+
+echo "<br>" ."Q. 1: To Check whether the current month is August or not?" ;
+
+$month = date('F');
+
+if (date('F' == "August")){
+    echo "<br>" ."It's August, so it's still holiday." ;
+}
+
+else{
+    echo "<br>" ."Not August, This is " . $month . " so i don't have any holidays.";
+}
+echo "<br>";
+echo "<br>" ."Q. 2: To Check if the color is red or not?" ;
+
+$color = "red";
+
+if ($color == "red"){
+    echo "<br>" ."The color is red.";
+}
+
+else {
+    echo "<br>" ."The color is not red.";
+}
+echo "<br>";
+
+echo "<br>" ."Q. 3: To Check the grades of the students based on the scale given in the question?" ;
+
+$gradeOfStudent = 85;
+
+if($gradeOfStudent >= 80){
+    echo "<br>" ."Your grade is: Excellent"; 
+}
+
+elseif ($gradeOfStudent >= 70 && $gradeOfStudent <= 80) {
+    echo "<br>" ."Your grade is: Great"; 
+    
+}
+
+elseif($gradeOfStudent >= 60 && $gradeOfStudent <= 70){
+    echo "<br>" ."Your grade is: Good"; 
+
+}
+
+elseif($gradeOfStudent >= 50 && $gradeOfStudent <= 60){
+    echo "<br>" ."Your grade is: Pass"; 
+
+}
+
+elseif($gradeOfStudent < 50){
+    echo "<br>" ."Sorry! You are Fail"; 
+
+}
+
+echo "<br>";
+
+echo "<br>" ."Q. 4: To Check if the user is able to vote?" ;
+
+
+
+
+?>
